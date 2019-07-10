9070f466f0f2126f9d227aa1a51d9fe66adb11bf
김민수 <min19828257@naver.com>
2019-07-11 06:52:03 +0900

    7월 11일
    쓰리쿠션 알고리즘추가적인 코드작성
    앞으로 x,y에 대해 각각 계산과정이 필요

--- 쓰리쿠션 여부판단 알고리즘2/check_cusion.py
+++ 쓰리쿠션 여부판단 알고리즘2/check_cusion.py
@@ -1,3 +1,8 @@
+#쿠션여부 카운팅
+class Cusion:
+    wall_cnt=0
+    ball_cnt=0
+
 #받은 데이터를 수구,적구2개,사각지점으로 분류하기
 def classify(data):
     data_ = data.split(",")
@@ -17,8 +22,47 @@ def classify(data):
 
 def cusion_check(soogoo,jukgoo1,jukgoo2,left_bottom_corner,left_top_corner,right_top_corner,right_bottom_corner):
     #1.수구 적구충돌여부확인
+    check_collision(soogoo,jukgoo1,jukgoo2)
     #2.사각지점 통해 수구가 벽을 3번맞췄는가
-    #3.쿠션의 조건이 맞나
-    result = "1"
+    check_wall(soogoo,left_bottom_corner,left_top_corner,right_top_corner,right_bottom_corner)
 
-    return result
+    #3.쿠션의 조건이 맞나 result 1(성공) -1(실패)
+    resullt = confirm()
+    
+    if(result == 1):
+        return 1
+    else:
+        return 2
+
+
+#1.수구 적구충돌여부확인
+def check_collision(soogoo,jukgoo1,jukgoo2):
+    distance1 = int(soogoo) - int(jukgoo1)
+    distance2 = int(soogoo) - int(jukgoo2)
+
+    if(distance1 == 0 | distance2 == 0):
+        Cusion.ball_cnt += 1
+
+#2.사각지점 통해 수구가 벽을 3번맞췄는가
+def check_wall(soogoo,left_bottom_corner,left_top_corner,right_top_corner,right_bottom_corner):
+    # 각각의 당구공 x,y좌표들을 계산해야
+    distance_soogoo_l_b_c = int(soogoo) - int(left_bottom_corner)
+    distance_soogoo_l_t_c = int(soogoo) - int(left_top_corner)
+    distance_soogoo_r_t_c = int(soogoo) - int(right_top_corner)
+    distance_soogoo_r_b_c = int(soogoo) - int(right_botoom_corner)
+
+    if(distance_soogoo_r_b_c == 0 | distance_soogoo_r_t_c == 0 | distance_soogoo_l_t_c==0 | distance_soogoo_l_b_c==0):
+        Cusion.wall_cnt += 1
+
+#3.쿠션의 조건이 맞나 result 1(성공) -1(실패)
+def confirm(self):
+    if(Cusion.ball_cnt > 1):
+        print("공 충돌2번 오케이")
+        if(Cusion.wall_cnt > 2):
+            print("벽 충돌3번 오케이 쓰리쿠션 성공")
+            Cusion.wall_cnt=0;Cusion.ball_cnt=0
+            return 1
+    else:
+        print("공 충돌 실패")
+        return -1
+
--- 쓰리쿠션 여부판단 알고리즘2/socket_server.py
+++ 쓰리쿠션 여부판단 알고리즘2/socket_server.py
@@ -1,15 +1,15 @@
 from socket import *
 import check_cusion
 
-def transform(data):
+#받아온 데이터를 수구,적구1,적구2,사각지점들로 분류하기
+def classi(data):
     sg,jg1,jg2,l_b_c,l_t_c,r_t_c,r_b_c = check_cusion.classify(data)
+    return sg,jg1,jg2,l_b_c,l_t_c,r_t_c,r_b_c
+
+#각각의 데이터를 이용해 쿠션 체크하기
+def Check(sg,jg1,jg2,l_b_c,l_t_c,r_t_c,r_b_c):
     result = check_cusion.cusion_check(sg,jg1,jg2,l_b_c,l_t_c,r_t_c,r_b_c)
     return result
-
-def Setting():
-    data = Data_base.Get()
-    print("DB data : ", DB.Get())
-    return data
 
 def socket_set(ip,port):
     serverSock = socket(AF_INET, SOCK_STREAM)
@@ -21,7 +21,9 @@ def socket_set(ip,port):
     print(str(addr),'에서 접속이 확인되었습니다.')
 
     data = connectionSock.recv(1024).decode('utf-8')
-    answer = transform(data)
+    
+    sg,jg1,jg2,l_b_c,l_t_c,r_t_c,r_b_c = classi(data)
+    answer = Check(sg,jg1,jg2,l_b_c,l_t_c,r_t_c,r_b_c)
 
     connectionSock.send(answer.encode('utf-8'))
     print('메시지를 보냈습니다.')
